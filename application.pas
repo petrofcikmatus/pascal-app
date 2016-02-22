f1c82ff29b502d659178c493a772f165eea5a032
Matúš Petrofčík <petrofcikmatus@gmail.com>
2016-02-22 22:04:22 +0100

    first steps

--- application.pas
+++ application.pas
@@ -1,1 +1,41 @@
+// Programovanie 24: Textové súbory, základné príkazy pre prácu s textovými súbormi. Napíšte program, ktorý:
+// 1. prekopíruje vytvorený textový súbor do nového súboru; 
+// 2. zmení všetky písmena na veľké.
+
 program application;
+
+const 
+INPUT_FILE = 'input.txt';
+OUTPUT_FILE = 'output.txt';
+
+var 
+input, output : text;
+line : string = '';
+
+begin
+
+ 	assign(input, inputFile);
+	reset(input);
+
+	assign(output, outputFile);
+	rewrite(output);
+
+	// Cyklus kým nedôjdeme na koniec input súboru.
+	while not eof(input) do  begin
+	   // Prečítanie jedného riadku z input súboru...
+	   readln(input,line);
+
+	   // ... a jeho zapísanie do output súboru.
+	   writeln(output,line);
+	end;
+
+	// Zatvorenie oboch súborov.
+	close(input);
+	close(output);
+
+	// Čakanie na stlačenie Enteru.
+	readln;
+
+	// Vypis, že kto program vytvoril :P
+	writeln('Vytvoril Matúš Petrofčík.');
+end.
