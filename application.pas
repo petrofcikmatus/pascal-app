5ffa2457120924ce373ffe77c386acfbc7e5f6a7
Matúš Petrofčík <petrofcikmatus@gmail.com>
2016-02-22 22:27:03 +0100

    final application

--- application.pas
+++ application.pas
@@ -2,40 +2,75 @@
 // 1. prekopíruje vytvorený textový súbor do nového súboru; 
 // 2. zmení všetky písmena na veľké.
 
-program application;
+// Názov programu.
+program application; 
 
+// Import potrebnej knižnice, ktorá obsahuje funkciu AnsiUpperCase().
+uses sysutils;
+
+// Definovanie konštánt, napr. názvy súborov.
 const 
-INPUT_FILE = 'input.txt';
-OUTPUT_FILE = 'output.txt';
+SRC_FILE_NAME = 'input.txt';
+DST_FILE_NAME = 'output.txt';
 
+// Definovanie premenných, napr. obsah súborov.
 var 
-input, output : text;
+file_src, file_dst : text;
 line : string = '';
 
+
+// Začiatok programu.
 begin
 
- 	assign(input, inputFile);
-	reset(input);
+	// Do premennej file_src priradíme zdrojový súbor...
+ 	assign(file_src, SRC_FILE_NAME);
 
-	assign(output, outputFile);
-	rewrite(output);
+ 	// ... a otvoríme ho pre čítanie.
+	reset(file_src);
 
-	// Cyklus kým nedôjdeme na koniec input súboru.
-	while not eof(input) do  begin
-	   // Prečítanie jedného riadku z input súboru...
-	   readln(input,line);
+	// Hláška, že sme otvorili zdrojový súbor.
+	writeln('Zdrojový súbor bol úspešne otvorený.');
 
-	   // ... a jeho zapísanie do output súboru.
-	   writeln(output,line);
+	// Do premennej file_dst priradíme cieľový súbor...
+	assign(file_dst, DST_FILE_NAME);
+
+	// ... a otvoríme ho pre zápis (prepísanie pôvodného obsahu, prípadne ho vytvoríme ak neexistuje).
+	rewrite(file_dst);
+
+	// Hláška, že sme vytvorili/vyčistili cieľový súbor.
+	writeln('Cieľový súbor bol úspešne vytvorený/vyčistený.');
+
+	// Cyklus while, kým neprejdeme celý zdrojový súbor.
+	while not eof(file_src) do begin
+	   // Prečítanie jedného riadku zo zdrojového súboru.
+	   readln(file_src, line);
+
+	   // Prekonvertovanie načítaného riadku na veľké písmená.
+	   line := AnsiUpperCase(line);
+
+	   // Zapísanie načítaného riadku do cieľového súboru.
+	   writeln(file_dst, line);
+
+	// Koniec cyklu while.
 	end;
 
-	// Zatvorenie oboch súborov.
-	close(input);
-	close(output);
+	// Hláška, že sa všetko poradilo.
+	writeln('Zdrojový súbor bol úspešne prekopírovaný do cieľového súboru.');
 
-	// Čakanie na stlačenie Enteru.
-	readln;
+	// Zatvorenie oboch súborov.
+	close(file_src);
+	close(file_dst);
+
+	// Hláška, že sa sme zatvorili súbory.
+	writeln('Zdrojový aj cieľový súbor boli zatvorené.');
 
 	// Vypis, že kto program vytvoril :P
+	writeln('-------------------------');
 	writeln('Vytvoril Matúš Petrofčík.');
+
+	// Čakanie na stlačenie Enteru.
+	writeln('Program ukončíte stlačením klávesy Enter.');
+	readln;
+
+// Koniec programu.
 end.
